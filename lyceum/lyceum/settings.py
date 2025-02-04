ea396b939372b93a37f03bcc66e30e3de06b4a47
Eugene <7zhhovalyg@mail.ru>
2025-02-05 00:23:30 +0300

    fff

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -16,9 +16,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # Разрешённые хосты (по умолчанию пустой список)
 if not DEBUG:
-    ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(
-        ","
-    )
+    ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
@@ -88,19 +86,13 @@ AUTH_PASSWORD_VALIDATORS = [
         ),
     },
     {
-        "NAME": (
-            "django.contrib.auth.password_validation.MinimumLengthValidator"
-        ),
+        "NAME": ("django.contrib.auth.password_validation.MinimumLengthValidator"),
     },
     {
-        "NAME": (
-            "django.contrib.auth.password_validation.CommonPasswordValidator"
-        ),
+        "NAME": ("django.contrib.auth.password_validation.CommonPasswordValidator"),
     },
     {
-        "NAME": (
-            "django.contrib.auth.password_validation.NumericPasswordValidator"
-        ),
+        "NAME": ("django.contrib.auth.password_validation.NumericPasswordValidator"),
     },
 ]
 
