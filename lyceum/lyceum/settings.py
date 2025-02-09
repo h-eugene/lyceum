84e75fef12775cc4bedb824fd376f28bbb1d5df7
Eugene <7zhhovalyg@mail.ru>
2025-02-09 14:17:32 +0300

    Изменить названия переменных окружения

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -7,11 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY", "default")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "default")
 
 DEBUG = os.getenv("DJANGO_DEBUG", "PROD_MODE")
 if DEBUG == "PROD_MODE":
-    ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1").split(",")
+    ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 
 INSTALLED_APPS = [
