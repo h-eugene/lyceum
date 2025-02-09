20ebd8b7ba0ff2b79b7b56555f3794cfc4ed1aca
Eugene <7zhhovalyg@mail.ru>
2025-02-09 14:40:01 +0300

    Добавить Django debug toolbar в проект

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -24,6 +24,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.messages",
     "homepage.apps.HomepageConfig",
+    # DEBUG
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -34,6 +36,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = "lyceum.urls"
--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -1,11 +1,20 @@
-from django.contrib import admin
-
+import django
 import django.urls
+from django.contrib import admin
 
 
 urlpatterns = [
     django.urls.path("", django.urls.include("homepage.urls")),
+    django.urls.path("about/", django.urls.include("about.urls")),
     django.urls.path("admin/", admin.site.urls),
     django.urls.path("catalog/", django.urls.include("catalog.urls")),
-    django.urls.path("about/", django.urls.include("about.urls"))
 ]
+
+if django.conf.settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += (
+        django.urls.path(
+            "__debug__/", django.urls.include(debug_toolbar.urls)
+        ),
+    )
