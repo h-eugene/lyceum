d73107cb4991f078757cffdcca5be5eaf7369759
Eugene <7zhhovalyg@mail.ru>
2025-02-20 23:42:46 +0300

    format

--- lyceum/catalog/migrations/0001_squashed_0003_alter_tag_options_alter_category_is_published_and_more.py
+++ lyceum/catalog/migrations/0001_squashed_0003_alter_tag_options_alter_category_is_published_and_more.py
@@ -10,7 +10,10 @@ class Migration(migrations.Migration):
     replaces = [
         ("catalog", "0001_initial"),
         ("catalog", "0002_alter_category_options_alter_item_options_and_more"),
-        ("catalog", "0003_alter_tag_options_alter_category_is_published_and_more"),
+        (
+            "catalog",
+            "0003_alter_tag_options_alter_category_is_published_and_more",
+        ),
     ]
 
     initial = True
