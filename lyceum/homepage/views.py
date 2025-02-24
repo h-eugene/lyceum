5e765d897bd21bd5fbfd16f68de644c76309945c
Eugene <7zhhovalyg@mail.ru>
2025-02-24 10:20:07 +0300

    add static files

--- lyceum/homepage/views.py
+++ lyceum/homepage/views.py
@@ -1,5 +1,11 @@
 import django.http
+import django.shortcuts
 
 
 def home(request):
     return django.http.HttpResponse("<body>Главная</body>")
+
+
+def index_render(request):
+    template = "homepage/home.html"
+    return django.shortcuts.render(request, template)
