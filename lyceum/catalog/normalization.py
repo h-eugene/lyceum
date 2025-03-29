a9dbe01c5422936cbe5a5a9331abb4dd5db1fc75
Eugene <7zhhovalyg@mail.ru>
2025-03-29 16:33:30 +0300

    upgrade nulrmalization

--- lyceum/catalog/normalization.py
+++ lyceum/catalog/normalization.py
@@ -6,8 +6,8 @@ from transliterate import translit
 def normalize_name(name):
     if not name:
         return ""
-    normalized = translit(name.lower(), "ru", reversed=True)
-    return re.sub(r"[^a-z0-9-]", "", normalized.strip())
+    normalized = translit(name.lower().strip(), "ru", reversed=True)
+    return re.sub(r"[^a-z0-9-а-я]", "", normalized)
 
 
 __all__ = []
